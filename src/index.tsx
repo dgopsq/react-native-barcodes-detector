b04d69d265ef345958bdcb05f9405dc5473476db
Diego Pasquali <5963683+dgopsq@users.noreply.github.com>
2022-05-20 19:34:44 +0200

    Add TS types

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,25 @@
 import { NativeModules, Platform } from 'react-native';
 
+/**
+ * Data structure describing the `x` and `y` position
+ * of a point on a 2D plane.
+ */
+export type Point = {
+  x: number;
+  y: number;
+};
+
+/**
+ * Data structure describing a single Barcode
+ * detected in an image.
+ */
+export type Barcode = {
+  format: number;
+  rawValue: string | null;
+  displayValue: string | null;
+  cornerPoints: Array<Point>;
+};
+
 const LINKING_ERROR =
   `The package 'react-native-barcodes-detector' doesn't seem to be linked. Make sure: \n\n` +
   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
@@ -17,6 +37,12 @@ const BarcodesDetector = NativeModules.BarcodesDetector
       }
     );
 
-export function scan(imageUrl: string): Promise<Array<unknown>> {
+/**
+ * Detect all the barcodes in the given image.
+ *
+ * @param imageUrl The url to the local image used in the detection process. This could be either with the `file://` protocol or without it.
+ * @returns A promise resolving to a list of `Barcode` detected in the image.
+ */
+export function scan(imageUrl: string): Promise<Array<Barcode>> {
   return BarcodesDetector.scan(imageUrl);
 }
