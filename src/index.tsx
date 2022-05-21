ce0aacae631fff9e6a5b3c44d997011fb9251f3e
Diego Pasquali <5963683+dgopsq@users.noreply.github.com>
2022-05-21 16:05:35 +0200

    Reorder `BarcodeFormat`

--- src/index.tsx
+++ src/index.tsx
@@ -14,21 +14,21 @@ export type Point = {
  * its identification number.
  */
 export enum BarcodeFormat {
+  UNKNOWN = -1,
   ALL_FORMATS = 0,
-  AZTEC = 4096,
-  CODABAR = 8,
   CODE_128 = 1,
   CODE_39 = 2,
   CODE_93 = 4,
+  CODABAR = 8,
   DATA_MATRIX = 16,
   EAN_13 = 32,
   EAN_8 = 64,
   ITF = 128,
-  PDF417 = 2048,
   QR_CODE = 256,
-  UNKNOWN = -1,
   UPC_A = 512,
   UPC_E = 1024,
+  PDF417 = 2048,
+  AZTEC = 4096,
 }
 
 /**
